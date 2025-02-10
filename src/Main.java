cafcbd64422851ffeda96b3b9e3e05284a8b58a3
hgold <goldsmhr@mail.uc.edu>
2025-02-10 11:41:55 -0500

    Task 02 Complete

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        int birthMonth = 0;
+            System.out.println("Enter your birth month number: ");
+            birthMonth = in.nextInt();
+                if (birthMonth >= 1 && birthMonth <= 12) {
+                    System.out.println("Your birth month is " + birthMonth);
+                } else System.out.println("You entered an incorrect month value: " + birthMonth);
     }
 }
